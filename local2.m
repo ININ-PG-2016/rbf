45e333052766418d9373b72763f737329b60b0df
hrudalu <hrudalu@students.zcu.cz>
2016-11-09 20:16:47 +0100

    local2 and local3 functions fixed

--- local2.m
+++ local2.m
@@ -1,7 +1,4 @@
 function ret = local2(r)
-  if r > 1
-    ret = 0;
-  else
-    ret = ((1 .- r).^3).*(3 .* r .+ 1);
-  end
+    alpha = 5;
+    ret = (r .* alpha <= 1) .* ((1 .- (r .* alpha)).^3).*(3 .* (r .* alpha) .+ 1) + ((r .* alpha) > 1) .* 0;
 endfunction
--- local3.m
+++ local3.m
@@ -1,8 +1,4 @@
 function ret = local3(r)
-  a = 10;
-  if ((a .* r) > 1)
-    ret = 0;
-  else
-    ret = ((1 .- (a .* r)).^5).*((8 .* (a .* r).^2) .+ 5 .* (a .* r) .+ 1);
-  end
-end
+    alpha = 10;
+    ret = (r .* alpha <= 1).*((1 .- (r .* alpha)).^5).*((8 .* (r .* alpha).^2) .+ 5 .* (r .* alpha) .+ 1) + ((r .* alpha) > 1) .* 0;
+endfunction
